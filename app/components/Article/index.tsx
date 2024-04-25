135eeee77b1784a6fa5ba19f0503655893ebcee9
NoA <noa20040831@gmail.com>
2024-04-26 02:13:26 +0900

    記事の作成/更新日時の表す画像追加

--- app/components/Article/index.tsx
+++ app/components/Article/index.tsx
@@ -1,4 +1,4 @@
-import { formatRichText } from '../../libs/utils';
+import parse  from 'html-react-parser'
 import { type Article } from '../../libs/microcms';
 import PublishedDate from '../Date';
 import TagList from '../TagList';
@@ -9,17 +9,17 @@ type Props = {
 
 export default function Article({ data }: Props) {
     return (
-        <main className="">
-            <h1 className="">{data.title}</h1>
+        <main className="flex flex-col items-center justify-center w-4/5 mx-auto bg-gray-900 m-8 rounded-lg">
+            <h1 className="text-3xl m-4 p-4 font-bold">{data.title}</h1>
             <TagList tags={data.tags} />
-            <p className="">{data.description}</p>
-            <PublishedDate date={data.publishedAt || data.createdAt} />
-            <div
-                className=""
-                dangerouslySetInnerHTML={{
-                    __html: `${formatRichText(data.content)}`,
-                }}
-            />
+            <p className="m-2">
+                {data.description}
+            </p>
+            <PublishedDate date={data.publishedAt} />
+            {(data.updatedAt && data.updatedAt !== data.publishedAt) && <PublishedDate date={data.updatedAt} />}
+            <div className="m-4 p-4">
+                {parse(data.content)}
+            </div>
         </main>
     );
 }
